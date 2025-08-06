10a0a64010ac34bb3c01f4bc8c30a2772a67857e
Nama Anda <email@anda.com>
2025-08-06 11:53:30 +0700

    tambahkan TodoApp

--- src/App.jsx
+++ src/App.jsx
@@ -22,6 +22,57 @@ function useLocalStorage(key, initialValue) {
   return [storedValue, setValue];
 }
 
+function TodoApp() {
+  const [todos, setTodos] = useLocalStorage('todos', []);
+  const [newTodo, setNewTodo] = useState('');
 
+  const addTodo = () => {
+    if (newTodo.trim() === '') return;
+    setTodos([...todos, { text: newTodo, completed: false }]);
+    setNewTodo('');
+  }
+
+  const toggleTodo = index => {
+    const newTodos = [...todos];
+    newTodos[index].completed = !newTodos[index].completed;
+    setTodos(newTodos)
+  }
+
+  const removeTodo = index => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1);
+    setTodos(newTodos);
+  };
+
+  return (
+    <div>
+      <h1>Todo List</h1>
+      {/* Input field to add new todo */}
+      <input
+        type="text"
+        value={newTodo}
+        onChange={e => setNewTodo(e.target.value)}
+      />
+      {/* Button to add new todo */}
+      <button onClick={addTodo}>Add Todo</button>
+      {/* Display list of todos */}
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            {/* Todo item with click handler to toggle completion */}
+            <span
+              style={{ textDecoration: todo.completed ? 'line-through' : 'none' }}
+              onClick={() => toggleTodo(index)}
+            >
+              {todo.text}
+            </span>
+            {/* Button to delete todo */}
+            <button onClick={() => removeTodo(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
 
 export default TodoApp;
